5f64b37a72f017dba4458997273fd7bc80965a38
Meme Man <tomekslawny@gmail.com>
2020-12-03 20:54:22 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import YoutubeForm from "./components/YoutubeForm";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <YoutubeForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+
+  console.log("Form values", formik.values);
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
